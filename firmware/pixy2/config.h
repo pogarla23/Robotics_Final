59ed0c677908898e8295f58ae2a65ac6a25cc0e9
pogarla23 <pgarla3@lsu.edu>
2025-11-14 14:46:59 -0600

    add config files

---
+++ firmware/pixy2/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define FIELD_WIDTH 600
+#define FIELD_HEIGHT 400
+
+#define PIXY_SIGNATURE 1            //hope to change later 
+#define CAMERA_OFFSET_X 0
+#define CAMERA_OFFSET_Y 0
